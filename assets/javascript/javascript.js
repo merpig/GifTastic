d9c7eebdc5871a45ab1bccbbe6fe45fce816575f
Sasha Peters <peteram1@sewanee.edu>
2019-03-02 02:38:13 -0600

    added download for gifs

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -14,6 +14,7 @@ var themes = [
     "iPhone"
 ];
 
+// Holds favorited gifs
 var favorites = [];
 
 // api variables
@@ -27,7 +28,6 @@ var gifCount = 0;
 // **Placed outside $(document).ready(...) so that new elements can access func
 function giphyThemes(element) {
     var btnTheme = $(element).text().toLowerCase();
-
     $.ajax({
         url: (queryURL+apiKey+btnTheme),
         method: "GET"
@@ -36,13 +36,10 @@ function giphyThemes(element) {
         gifCount++;
         var giphyLink = response.data.image_url;
         var giphyTitle= response.data.title;
-
         var uniqueId = "gif" + gifCount;
-
-
         var gifDiv = $(".giphyDiv");
-        var gifRap = $("<div class='row'>");
-        var favDiv = $("<div class='col-sm-3 text-center favDiv'>").text("Fav");
+        var gifRap = $("<div class='row parent'>");
+        var favDiv = $("<div class='col-sm-3 text-center favDiv'>");
         var imgDiv = $("<div class='col-sm-9 imgDiv'>").text(giphyTitle);
         var imgTag = $("<img>").attr(
             {   
@@ -50,62 +47,70 @@ function giphyThemes(element) {
                 "class" : "favCheckBox",
                 "id"    : uniqueId
             });
-
-        /* append favorite button to this div */
-
-        var favBtn = $("<input class='star' onclick='fav(this)' type='checkbox' name='favorite' value='" + uniqueId + "'>");
+        var favBtn = $("<div class='starDiv'><input class='star' onclick='fav(this)' type='checkbox' name='favorite' value='" + uniqueId + "'></div>");
+        var savBtn = $("<div class='saveDiv'><button id='" + giphyLink + "'onclick='downLoad(this)'> <i class='fa fa-download' ></i></button></div>")
         favDiv.append(favBtn);
-
-        /*                                  */
-
+        favDiv.append(savBtn);
         gifRap.append("<hr>");
         gifRap.append(favDiv);
-        //gifRap.append("<vl>");
         gifRap.append(imgDiv.append(imgTag));
-        
         gifDiv.prepend(gifRap);
     });  
+}
+
+function downLoad(element){
+    $.ajax({
+        url: element.id,
+        method: 'GET',
+        xhrFields: {
+            responseType: 'blob'
+        },
+        success: function (data) {
+            var a = document.createElement('a');
+            var url = window.URL.createObjectURL(data);
+            a.href = url;
+            a.download = element.id;
+            a.click();
+            window.URL.revokeObjectURL(url);
+        }
+    });
 }
 
 // Allows use to favorite and save images in local storage
 // **Placed outside $(document).ready(...) so that new elements can access func
 function fav(element){
     event.preventDefault();
-
     var imgID = $(element).val();
     var imgTag = $("#" + imgID)[0];
     imgSrc = imgTag.src
-
     if(!localStorage.getItem(imgSrc)){
         localStorage.setItem(imgSrc,imgSrc);
         favorites.push(imgSrc);
         localStorage.setItem("favoriteList",JSON.stringify( favorites ));
         $(".gifFavDiv").prepend("<img src='" + imgSrc + "'>");
     }
-
 }
 
+// Function to display any favorited gifs
 function generateFavs(){
     if(localStorage.getItem("favoriteList")){
         favorites = JSON.parse(localStorage.getItem("favoriteList"));
-
         $.each(favorites, function(i){
             $(".gifFavDiv").prepend("<img src='" + favorites[i] + "'>");
         });
     }
 }
 
-
 $(document).ready(function() {
     
+    // generate fav gifs
     generateFavs();
     
+    // Function to create buttons
     function generateBtns(arr){
         for(var i = 0; i < arr.length; i++){
-
             var themeBtnsDiv = $(".theme-btns");
             var tempBtn;
-
             if(arr.length>1)
                 tempBtn = $("<button>")
                     .attr({
@@ -119,33 +124,24 @@ $(document).ready(function() {
                         "class"   : "btn btn-default btnStyle",
                         "onclick" : "giphyThemes(this)"
                         })
-                    .text(arr[i]);
-            
+                    .text(arr[i]);     
             themeBtnsDiv.prepend(tempBtn);
-
         }
     }
 
+    // generate buttons
     generateBtns(themes);
 
+    // onclick event to add a new button for new themes
     $("#add-theme").on("click", function(event) 
     {
-        event.preventDefault();
-    
+        event.preventDefault(); 
         var inputTheme = $("#input-theme");
-        var newTheme = [inputTheme.val()];
-        
+        var newTheme = [inputTheme.val()];       
         if(newTheme[0] !== ""){
             themes.push( newTheme );
             generateBtns(themes[themes.length-1]);
         }
-
         inputTheme.val("");
     });
-
-    
-
-
-
 })
-
