434daa244c2bea6f074e6d2b4eceee6d7ff3dd7f
Sasha Peters <peteram1@sewanee.edu>
2019-03-02 00:39:19 -0600

    full site added

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,3 +1,4 @@
+// The default set of meme themes
 var themes = [
     "Soccer",
     "Gaming",
@@ -13,31 +14,92 @@ var themes = [
     "iPhone"
 ];
 
+var favorites = [];
 
+// api variables
+var apiKey = "api_key=QCUadDTpWvHZzMUjNKjEqKCvZvOKcy9O&limit=1&tag=";
+var queryURL ="https://api.giphy.com/v1/gifs/random?";
 
-var apiKey = "api_key=m9Tkfcf0aYqCDBLLxrx9C3429jX4IH4x&limit=1&tag=";
-var queryURL ="http://api.giphy.com/v1/gifs/random?";
+// gif counter to ensure unique id's
+var gifCount = 0;
 
+// Contains the ajax call for giphy api.
+// **Placed outside $(document).ready(...) so that new elements can access func
 function giphyThemes(element) {
-    var btnTheme = $(element).text();
+    var btnTheme = $(element).text().toLowerCase();
 
     $.ajax({
-        url: queryURL+apiKey+btnTheme,
+        url: (queryURL+apiKey+btnTheme),
         method: "GET"
     }).then(function(response) {
+        console.log(response);
+        gifCount++;
+        var giphyLink = response.data.image_url;
+        var giphyTitle= response.data.title;
 
-        giphyLink = response.data.image_url;
+        var uniqueId = "gif" + gifCount;
+
+
         var gifDiv = $(".giphyDiv");
-        var imgTag = $("<img>").attr("src", giphyLink);
-        gifDiv.append(imgTag);
+        var gifRap = $("<div class='row'>");
+        var favDiv = $("<div class='col-sm-3 text-center favDiv'>").text("Fav");
+        var imgDiv = $("<div class='col-sm-9 imgDiv'>").text(giphyTitle);
+        var imgTag = $("<img>").attr(
+            {   
+                "src"   : giphyLink,
+                "class" : "favCheckBox",
+                "id"    : uniqueId
+            });
+
+        /* append favorite button to this div */
+
+        var favBtn = $("<input class='star' onclick='fav(this)' type='checkbox' name='favorite' value='" + uniqueId + "'>");
+        favDiv.append(favBtn);
+
+        /*                                  */
+
+        gifRap.append("<hr>");
+        gifRap.append(favDiv);
+        //gifRap.append("<vl>");
+        gifRap.append(imgDiv.append(imgTag));
+        
+        gifDiv.prepend(gifRap);
     });  
 }
 
+// Allows use to favorite and save images in local storage
+// **Placed outside $(document).ready(...) so that new elements can access func
+function fav(element){
+    event.preventDefault();
+
+    var imgID = $(element).val();
+    var imgTag = $("#" + imgID)[0];
+    imgSrc = imgTag.src
+
+    if(!localStorage.getItem(imgSrc)){
+        localStorage.setItem(imgSrc,imgSrc);
+        favorites.push(imgSrc);
+        localStorage.setItem("favoriteList",JSON.stringify( favorites ));
+        $(".gifFavDiv").prepend("<img src='" + imgSrc + "'>");
+    }
+
+}
+
+function generateFavs(){
+    if(localStorage.getItem("favoriteList")){
+        favorites = JSON.parse(localStorage.getItem("favoriteList"));
+
+        $.each(favorites, function(i){
+            $(".gifFavDiv").prepend("<img src='" + favorites[i] + "'>");
+        });
+    }
+}
+
+
 $(document).ready(function() {
     
-
-    // The default set of meme themes
-
+    generateFavs();
+    
     function generateBtns(arr){
         for(var i = 0; i < arr.length; i++){
 
@@ -59,7 +121,7 @@ $(document).ready(function() {
                         })
                     .text(arr[i]);
             
-            themeBtnsDiv.prepend(tempBtn).append($("<br>"));
+            themeBtnsDiv.prepend(tempBtn);
 
         }
     }
@@ -80,6 +142,10 @@ $(document).ready(function() {
 
         inputTheme.val("");
     });
+
+    
+
+
 
 })
 
